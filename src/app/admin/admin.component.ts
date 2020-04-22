a26808bf3c70194a6b96a50bdb2ca4a539bdf44a
C Venugopal <venugopal.c@a3ventures.org>
2020-04-22 14:48:46 +0530

    add all files

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService } from '../_services';
+import { ServiceService } from '../service.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({ templateUrl: 'admin.component.html' })
+export class AdminComponent implements OnInit {
+    loading = false;
+    users: User[] = [];
+id=''
+    constructor(private userService: UserService,
+        private service:ServiceService,
+    private route: ActivatedRoute) { }
+
+    ngOnInit() {
+       
+   this.id = this.service.getProduct(this.route.snapshot.params["name"])
+
+    }
+}
